e12854dba89ea1c3c453aa3dac81e267c5915aad
Evan Chiang <happyday9837@gmail.com>
2022-08-28 18:15:22 +0800

    update 8/28 homepage

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,106 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <nav>
+    <div class="top-nav d-flex justify-content-between container-lg">
+      <h2 class="mb-0 text-mainred fw-bold d-flex align-items-center">
+        <a href="#" class="text-decoration-none text-mainred d-block lh-base"
+          >饗農</a
+        >
+      </h2>
+      <ul class="list-unstyled d-flex mb-0 align-items-center">
+        <li class="">
+          <a href="#" class="navbtn d-block p-3 text-decoration-none fw-bold"
+            >關於饗農</a
+          >
+        </li>
+        <li class="">
+          <a href="#" class="navbtn d-block p-3 text-decoration-none fw-bold"
+            >饗農地圖</a
+          >
+        </li>
+        <li class="">
+          <a href="#" class="navbtn d-block p-3 text-decoration-none fw-bold"
+            >響農商城</a
+          >
+        </li>
+        <li class="">
+          <a
+            href="#"
+            class="navbtn d-block py-3 px-2 text-decoration-none fw-bold"
+            ><i class="material-icons d-block fs-5"> person </i></a
+          >
+        </li>
+        <li class="">
+          <a
+            href="#"
+            class="navbtn d-block py-3 px-2 text-decoration-none fw-bold"
+            ><i class="material-icons d-block fs-5"> shopping_cart </i></a
+          >
+        </li>
+      </ul>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="bottom-nav bg-mainred">
+      <div class="d-flex justify-content-end container-lg py-2">
+        <div class="input-group search-box">
+          <label class="input-group-text bg-white" for="itemSearch">
+            <span class="material-icons"> search </span>
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="itemSearch"
+            aria-label="search"
+            name="search"
+            placeholder="搜尋商品"
+          />
+          <button
+            class="btn btn-outline-secondary fw-bolder p-1 bg-mainyellow text-mainred"
+            type="button"
+            id="itemSearch"
+          >
+            搜尋商品
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div class="banner">
+    <!-- <img class="banner-img" src="./assets/images/bannerbanner.png" alt="封面" /> -->
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style lang="scss">
+* {
+  outline: 1px solid #000;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Microsoft JhengHei", Roboto, "Helvetica Neue", Arial, sans-serif;
 }
 
-nav {
+.navbtn {
+  color: #460303;
+  &:hover {
+    transition: 0.2s;
+    color: #fde47f;
+  }
+}
+.banner {
+  background-image: url("./assets/images/bannerbanner (2).png");
+  background-position: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  max-width: 100%;
+  height: calc(100vh - 110px);
+}
+
+.banner-img {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.search-box {
+  max-width: 356px;
 }
 </style>
