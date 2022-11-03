4ebfb1654ca1a7fcda89979f2e820232290dd6b8
Evan Chiang <happyday9837@gmail.com>
2022-11-04 00:50:05 +0800

    added - 商品分頁功能建立(圖片swiper、部分內容置入)

--- src/views/FarmProductView.vue
+++ src/views/FarmProductView.vue
@@ -1,26 +1,7 @@
 <script setup>
 import axios from 'axios';
-import { onMounted, reactive, ref } from 'vue';
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
-// const route = useRoute();
-
-// const pathId = route.params.id;
-// const product = reactive([]);
-// const getProduct = () => {
-//   axios
-//     .get(
-//       `${import.meta.env.VITE_APP_URL}api/${
-//         import.meta.env.VITE_APP_PATH
-//       }/product/${pathId}`,
-//     )
-//     .then((res) => {
-//       // console.log(route.params.id);
-//       // console.log(res.data);
-//       product.values = res.data.product;
-//       console.log('product.values', product.values);
-//     });
-// };
-
 import { Swiper, SwiperSlide } from 'swiper/vue';
 
 // Import Swiper styles
@@ -30,114 +11,282 @@ import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 import { FreeMode, Navigation, Thumbs } from 'swiper';
 
-const modules = [Thumbs, FreeMode, Navigation];
+const route = useRoute();
 
-const bigImg = reactive([
-  'https://t7.baidu.com/it/u=3165657288,4248157545&fm=193&f=GIF',
-  'https://t7.baidu.com/it/u=2942499027,2479446682&fm=193&f=GIF',
-  'https://t7.baidu.com/it/u=2610975262,3538281461&fm=193&f=GIF',
-  'https://t7.baidu.com/it/u=4138158235,3956816634&fm=193&f=GIF',
-  'https://t7.baidu.com/it/u=2942499027,2479446682&fm=193&f=GIF',
-  'https://t7.baidu.com/it/u=2610975262,3538281461&fm=193&f=GIF',
-  'https://t7.baidu.com/it/u=4138158235,3956816634&fm=193&f=GIF',
-]);
-const thumbsSwiper2 = ref(null);
+const pathId = route.params.id;
+const product = ref({
+  imgUrl: '',
+  imagesUrl: [''],
+  title: '',
+  category: '',
+  unit: '',
+  origin_price: 0,
+  price: 0,
+  aria: '',
+  farm: '',
+  description: '',
+  content: '',
+  is_enabled: 1,
+  is_special: 0,
+});
 
-const setThumbsSwiper2 = (swiper) => {
-  thumbsSwiper2.value = swiper;
+const getProduct = () => {
+  axios
+    .get(
+      `${import.meta.env.VITE_APP_URL}api/${
+        import.meta.env.VITE_APP_PATH
+      }/product/${pathId}`,
+    )
+    .then((res) => {
+      console.log(route.params.id);
+      console.log(res.data);
+      product.value = res.data.product;
+      console.log('product.value', product.value);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
+getProduct();
 
+const modules = [Thumbs, FreeMode, Navigation];
+const thumbsSwiper = ref(null);
+
+const setThumbsSwiper = (swiper) => {
+  thumbsSwiper.value = swiper;
+};
 </script>
 
 <template>
-  <div class="bread-mt container-lg pt-16 mt-32">
+  <div class="bread-mt container-lg pt-16 mt-80">
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb">
-        <li class="breadcrumb-item" aria-current="page">\饗農商城</li>
+        <li class="breadcrumb-item" aria-current="page">
+          首頁>饗農商城>產品頁
+        </li>
       </ol>
     </nav>
   </div>
+  <div class="container-lg">
+    <div class="row">
+      <div class="col-md-5">
+        <div class="sticky-top">
+          <swiper
+            :style="{
+              '--swiper-navigation-color': '#fff',
+              '--swiper-pagination-color': '#fff',
+            }"
+            :spaceBetween="10"
+            :navigation="true"
+            :thumbs="{ swiper: thumbsSwiper }"
+            :modules="modules"
+            class="mySwiper2"
+          >
+            <swiper-slide>
+              <img
+                class="productImgShow"
+                :src="product.imgUrl"
+                :alt="`${product.title}主圖`"
+              />
+            </swiper-slide>
+            <swiper-slide
+              v-for="(item, index) in product.imagesUrl"
+              :key="index + 23145"
+            >
+              <img class="productImgShow"
+              :src="item"
+              :alt="`${product.title}+${index}`"/>
+            </swiper-slide>
+          </swiper>
+          <swiper
+            @swiper="setThumbsSwiper"
+            :spaceBetween="10"
+            :slidesPerView="4"
+            :freeMode="true"
+            :watchSlidesProgress="true"
+            :modules="modules"
+            class="mySwiper"
+          >
+            <swiper-slide>
+              <img class="productImgBar" :src="product.imgUrl" />
+            </swiper-slide>
+            <swiper-slide
+              v-for="(item, index) in product.imagesUrl"
+              :key="index + 38290189"
+            >
+              <img class="productImgBar" :src="item" />
+            </swiper-slide>
 
-  <swiper
-    :style="{
-      '--swiper-navigation-color': '#fff',
-      '--swiper-pagination-color': '#fff',
-    }"
-    :spaceBetween="10"
-    :thumbs="{ swiper: thumbsSwiper2 }"
-    :modules="modules"
-    class="mySwiper2"
-    :loop="true"
-    :navigation="true"
-  >
-    <swiper-slide v-for="(item, index) of bigImg" :key="index">
-      <img class="img" :src="item" />
-    </swiper-slide>
-  </swiper>
+          </swiper>
+        </div>
+      </div>
+      <div class="col-md-7">
+        <h1></h1>
+        <div class="describe-box">
+          <p>
+            ★紫錐菊定期補充顯著提升保護力、調整體質<br />
+            ★【專利紫錐萃取＋耐熱益生菌＋維他命Ｃ】<br />
+            ★每包含「總咖啡酸衍生物3570μg」<br />
+            ★使用美國有機『EMUNEPOWER專利紫錐菊』萃取<br />
+            ★無添加人工香料、色素、防腐劑 ★無農藥、無西藥、無重金屬、無塑化劑<br />
+          </p>
+        </div>
+        <h3 class="price">$1000<span>/每公斤</span></h3>
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+              <button
+                class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseOne"
+                aria-expanded="true"
+                aria-controls="panelsStayOpen-collapseOne"
+              >
+                優惠活動
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseOne"
+              class="accordion-collapse collapse show"
+              aria-labelledby="panelsStayOpen-headingOne"
+            >
+              <div class="accordion-body">
+                <strong>優惠折扣中</strong>
+              </div>
+            </div>
 
-  <swiper
-    @swiper="setThumbsSwiper2"
-    :loop="true"
-    :spaceBetween="10"
-    :slidesPerView="5"
-    :freeMode="true"
-    :watchSlidesProgress="true"
-    :modules="modules"
-    class="mySwiper"
-  >
-    <swiper-slide v-for="(item, index) of bigImg" :key="index">
-      <img class="img" :src="item" />
-    </swiper-slide>
+            <div
+              class="
+                row row-cols-2
+                justify-content-around
+                align-items-center
+                mt-4
+              "
+            >
+              <div class="">
+                <div class="input-group">
+                  <button
+                    class="btn btn-outline-mainred"
+                    type="button"
+                    id="button-addon1"
+                  >
+                    -
+                  </button>
+                  <input
+                    type="number"
+                    disabled
+                    class="form-control text-center"
+                    placeholder=""
+                    aria-label="Example text with button addon"
+                    aria-describedby="button-addon1"
+                    value="1"
+                  />
+                  <button
+                    class="btn btn-outline-mainred"
+                    type="button"
+                    id="button-addon1"
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <div class="div text-center">
+                <button class="btn btn-mainred text-mainyellow w-100">
+                  加入購物車
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button
+              class="nav-link active"
+              id="itemDescriptionTab"
+              data-bs-toggle="tab"
+              data-bs-target="#itemDescription"
+              type="button"
+              role="tab"
+              aria-controls="itemDescription"
+              aria-selected="true"
+            >
+              商品介紹
+            </button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button
+              class="nav-link"
+              id="itemContentTab"
+              data-bs-toggle="tab"
+              data-bs-target="#itemContent"
+              type="button"
+              role="tab"
+              aria-controls="itemContent"
+              aria-selected="false"
+            >
+              產品內容
+            </button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button
+              class="nav-link"
+              id="itemReturnTab"
+              data-bs-toggle="tab"
+              data-bs-target="#itemReturn"
+              type="button"
+              role="tab"
+              aria-controls="itemReturn"
+              aria-selected="false"
+            >
+              退換貨須知
+            </button>
+          </li>
+        </ul>
 
-  </swiper>
-
-  <!-- <swiper
-    :spaceBetween="10"
-    :thumbs="{ swiper: thumbsSwiper }"
-    :modules="modules"
-    class="mySwiper2"
-    :navigation="{
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }"
-  >
-  <div class="swiper-button-next swiper-button-white"></div>
-  <div class="swiper-button-prev swiper-button-black"></div>
-    <swiper-slide v-for="(item, index) of bigImg" :key="index">
-      <img class="img" :src="item" />
-    </swiper-slide>
-  </swiper>
-  <swiper
-    @swiper="setThumbsSwiper"
-    :spaceBetween="10"
-    :slidesPerView="3"
-    :freeMode="true"
-    :watchSlidesProgress="true"
-    :modules="modules"
-    class="mySwiper"
-  >
-    <swiper-slide v-for="(item, index) of bigImg" :key="index">
-      <img class="img" :src="item" />
-    </swiper-slide>
-  </swiper> -->
-
+        <div class="tab-content">
+          <div
+            class="tab-pane active"
+            id="itemDescription"
+            role="tabpanel"
+            aria-labelledby="itemDescriptionTab"
+          >
+            商品介紹
+          </div>
+          <div
+            class="tab-pane"
+            id="itemContent"
+            role="tabpanel"
+            aria-labelledby="itemContentTab"
+          >
+            產品內容
+          </div>
+          <div
+            class="tab-pane"
+            id="itemReturn"
+            role="tabpanel"
+            aria-labelledby="itemReturnTab"
+          >
+            退換貨須知
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped>
+* {
+  outline: 1px solid #000;
+}
 .bread-mt {
   margin-top: 82px;
 }
-body {
-  position: relative;
-  height: 100%;
-}
 
-body {
-  background: #eee;
-  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-  font-size: 14px;
-  color: #000;
-  margin: 0;
-  padding: 0;
+.productImgShow {
+  width: 100%;
+  height: 350px;
+  object-fit: cover;
+  object-position: center center;
 }
 
 .swiper {
@@ -163,18 +312,6 @@ body {
   -ms-flex-align: center;
   -webkit-align-items: center;
   align-items: center;
-}
-
-.swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-body {
-  background: #000;
-  color: #000;
 }
 
 .swiper {
@@ -210,10 +347,10 @@ body {
   opacity: 1;
 }
 
-.swiper-slide img {
+.productImgBar {
   display: block;
   width: 100%;
-  height: 100%;
+  height: 75px;
   object-fit: cover;
 }
 </style>
