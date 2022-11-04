b968e0c0dc67f189f9ef040964441d5f8014ad7b
Evan Chiang <happyday9837@gmail.com>
2022-11-05 03:21:34 +0800

    fixed-預設產品資料初始化

--- src/stores/saveData.js
+++ src/stores/saveData.js
@@ -2,7 +2,22 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useDataStore = defineStore('dataSave', () => {
-  const productData = ref(null);
+  const productData = ref([{
+    imgUrl: '',
+    imagesUrl: [''],
+    title: '',
+    category: '',
+    unit: '',
+    origin_price: 0,
+    price: 0,
+    aria: '',
+    farm: '',
+    description: '',
+    content: '',
+    is_enabled: 1,
+    is_special: 0,
+  }]);
+
   const pagination = ref({
     total_pages: 1,
     current_page: 1,
@@ -10,6 +25,7 @@ export const useDataStore = defineStore('dataSave', () => {
     has_next: 1,
     category: '',
   });
+
   return {
     productData,
     pagination,
