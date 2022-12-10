0dc3fba1654e2fb6a726e8c6a0f79752fcd233fb
Evan Chiang <happyday9837@gmail.com>
2022-12-11 06:57:00 +0800

    updated-更新navbar&footer位置

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,8 +1,22 @@
 <script setup>
 import LayoutNavbar from '@/components/LayoutNavbar.vue';
+import LayoutFooter from '@/components/LayoutFooter.vue';
+import { onMounted, onUpdated } from 'vue';
+
+import { useCartApi } from '../composables/useCartApi';
+
+const { getCartData } = useCartApi();
+onMounted(() => {
+  getCartData();
+});
+onUpdated(() => {
+  getCartData();
+});
+
 </script>
 <template>
   <LayoutNavbar />
   <router-view></router-view>
+  <LayoutFooter />
 </template>
 <style></style>
