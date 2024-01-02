ac0bdc93c158bfdd30abc5cb3ca452cb2ec8968b
piskesocute <happyday9837@gmail.com>
2024-01-03 03:55:21 +0800

    refactor: 移除無用程式碼

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
-<script setup>
-// console.log('環境變數=>', import.meta.env);
-</script>
+<script setup></script>
 
 <template>
   <router-view> </router-view>
 </template>
 
 <style lang="scss">
-// * {
-//   outline: 1px solid #000;
-// }
 // 全站字體
 body {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
