dbaa1e1ec3edc04ba6ac739ea2d6982ee4cbbed6
piskesocute <happyday9837@gmail.com>
2024-01-03 03:56:40 +0800

    fix: 跑版問題

--- src/views/DashboradLogin.vue
+++ src/views/DashboradLogin.vue
@@ -31,32 +31,22 @@ onMounted(() => {
 });
 </script>
 <template>
-  <div class="w-100 login-bg" @keyup.enter="login">
+  <div
+    class="w-100 login-bg"
+    @keyup.enter="login"
+  >
     <div class="container-lg h-100">
       <div
-        class="
-          login-box
-          row
-          w-100
-          h-100
-          justify-content-center
-          align-items-center
-        "
+        class="login-box row w-100 h-100 justify-content-center align-items-center"
       >
         <div
-          class="
-            col-5
-            bg-white
-            pt-5
-            d-flex
-            justify-content-center
-            flex-wrap
-            rounded-12
-          "
+          class="col-5 gap-3 bg-white py-24 px-48 d-flex justify-content-center flex-wrap rounded-12"
         >
           <h1 class="mb-24">後台登入</h1>
-          <div class="mb-3 w-75" >
-            <label for="exampleFormControlInput1" class="form-label"
+          <div class="mb-3 w-100">
+            <label
+              for="exampleFormControlInput1"
+              class="form-label"
               >帳號</label
             >
             <input
@@ -67,30 +57,37 @@ onMounted(() => {
               v-model="user.username"
             />
           </div>
-          <div class="mb-5 w-75">
-            <label for="adminLoginPassword" class="form-label">密碼</label>
+          <div class="mb-5 w-100">
+            <label
+              for="adminLoginPassword"
+              class="form-label"
+              >密碼</label
+            >
             <input
-              type="email"
+              type="password"
               class="form-control"
               id="adminLoginPassword"
               v-model="user.password"
-
             />
           </div>
-          <button
-            type="button"
-            class="btn btn-mainred text-white fw-bold px-5 mb-32"
-            @click="login"
-
-          >
-            登入
-          </button>
+          <div class="w-100 text-center">
+            <button
+              type="button"
+              class="btn btn-mainred text-white fw-bold"
+              @click="login"
+            >
+              登入
+            </button>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-<style lang='scss' scoped>
+<style lang="scss" scoped>
+* {
+  outline: 1px solid;
+}
 .login-bg {
   height: 100vh;
   background-image: url('../assets/images/about-footer-bg.png');
