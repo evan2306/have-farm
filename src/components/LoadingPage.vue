7d3ee81755395dabe54b939c79eb6cfd6747690f
piskesocute <happyday9837@gmail.com>
2024-01-03 03:55:46 +0800

    feat: 新增loading animate page

---
+++ src/components/LoadingPage.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+  <div class="d-flex justify-content-center align-items-center vh-100">
+    <div
+      class="spinner-border text-primary"
+      role="status"
+    >
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped></style>
